81c0769225aec22048782721c220a4d4f9c2b04c
Basil V <bveerman@uvic.ca>
2011-07-06 09:25:50 -0700

    Changed nautilus script names

--- PyShare0.6.2/install.sh
+++ PyShare0.6.2/install.sh
@@ -5,9 +5,9 @@
 
 PWD="`pwd`"
 
-ln -s "${PWD}/PyShare_GTK.py" ~/.gnome2/nautilus-scripts/PyShare_GTK
-ln -s "${PWD}/sendFiles" ~/.gnome2/nautilus-scripts/sendFiles
-ln -s "${PWD}/sendDesktopScreenshot" ~/.gnome2/nautilus-scripts/sendDesktopScreenshot
-ln -s "${PWD}/sendWindowScreenshot" ~/.gnome2/nautilus-scripts/sendWindowScreenshot
+ln -s "${PWD}/PyShare_GTK.py" ~/.gnome2/nautilus-scripts/PyShare_GUI
+ln -s "${PWD}/sendFiles" ~/.gnome2/nautilus-scripts/PyShare_sendFiles
+ln -s "${PWD}/sendDesktopScreenshot" ~/.gnome2/nautilus-scripts/PyShare_SendDesktopScreenshot
+ln -s "${PWD}/sendWindowScreenshot" ~/.gnome2/nautilus-scripts/PyShare_sendWindowScreenshot
 
 
